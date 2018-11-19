3d609e5091d5f825b4c1a1099d366768acf5f6cf
Shweta <shweta.p1992@gmail.com>
2018-11-19 23:56:24 +0530

    repo setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Profile from './Profile/Profile.js';
+import './normalize.css';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<Profile />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
